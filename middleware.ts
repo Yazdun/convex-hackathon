7c56d3c958586750cce2a838d885dc6f2c8e6a42
Yaz <yazduntube@gmail.com>
2025-09-20 17:39:42 +0330

    fix redirect issue

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ import {
 } from "@convex-dev/auth/nextjs/server";
 
 const isAuthPage = createRouteMatcher([
+  "/",
   "/get-started",
   "/login",
   "/reset-password",
@@ -14,7 +15,8 @@ const isProtectedRoute = createRouteMatcher(["/chats"]);
 export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
   const isAuthenticated = await convexAuth.isAuthenticated();
 
-  if (isAuthPage(request) && isAuthenticated) return redirect(request, "/");
+  if (isAuthPage(request) && isAuthenticated)
+    return redirect(request, "/chats");
 
   if (isProtectedRoute(request) && !isAuthenticated)
     return redirect(request, "/login");
